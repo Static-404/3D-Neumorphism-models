ac0f51eab187823411228a8a62b02db827c4dfd8
Static-404 <pboybams16@gmail.com>
2023-08-29 12:53:09 +0100

    init

---
+++ lib/pages/light-neu-screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LightNeu extends StatelessWidget {
+  const LightNeu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Icon(Icons.apple, size: 100,),
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            boxShadow: [
+              BoxShadow(
+              color: Colors.grey,
+              offset: Offset(5.0, 5.0),
+              blurRadius: 15.0,
+              spreadRadius: 1.0,
+              ),
+              BoxShadow(
+                  offset: Offset(-5.0, -5.0),
+                  blurRadius: 15.0,
+                  spreadRadius: 1.0,
+                  color: Colors.white
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
